b2459b87b70c8e6fa6d0dd096a7280a6deb920e9
PiotrCzyz <sabath91@gmail.com>
2017-11-21 23:01:07 +0100

    testing dimensions input - happy path

--- src/test/java/com/epam/initgame/DataInitializerTest.java
+++ src/test/java/com/epam/initgame/DataInitializerTest.java
@@ -1,16 +1,39 @@
 package com.epam.initgame;
 
+import com.epam.utils.Settings;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
 
 import static org.testng.Assert.*;
 
 @Test
 public class DataInitializerTest {
 
-    public void descriptionNameGoesHere(){
-    //given
-    //when
-    //then
+    @DataProvider(name = "boardDimensions")
+    public Object[][] createBoardDimensions() {
+        return new Object[][]{
+                {4, 4},
+                {50, 3},
+                {4, 45}
+        };
+
+    }
+
+    @Test(dataProvider = "boardDimensions")
+    public void shouldBeAbleToCreateProperDimensions(int xSize, int ySize) {
+        //given
+        ByteArrayInputStream in = new ByteArrayInputStream((xSize + "\n" + ySize).getBytes());
+        System.setIn(in);
+        DataInitializer dataInitializer = new DataInitializer();
+
+        //when
+        dataInitializer.createBoardDimensions();
+
+        //then
+        assertEquals(dataInitializer.getSettings().getDimensions().getxSize(),xSize );
+        assertEquals(dataInitializer.getSettings().getDimensions().getySize(),ySize );
     }
 
 }
